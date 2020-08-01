904f7a6bf78e385982eeee63fe62231950722ee5
Tonin Neri <tondev@pop-os.localdomain>
2020-08-01 12:59:41 -0300

    Create new Pages

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import Logo from '../../assets/tonflixblue.png';
 import './Menu.css';
 import Button from '../Button';
@@ -8,10 +9,10 @@ import Button from '../Button';
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
            <img className="Logo" src={Logo} alt="TonFlix"/>
-            </a>
-            <Button as="a" className="ButtonLink" href="/">
+            </Link>
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo               
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Home from "./pages/Home";
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import CadastroVideo from './pages/cadastro/Video';
 
 
-import { BrowserRouter } from 'react-router-dom';
+const Pagina404 = () => (<div>Pagina 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+  <Switch>
+    <Route path="/cadastro/video" component={CadastroVideo} />
+    <Route path="/cadastro/categoria" component={CadastroCategoria} />
+    <Route path="/" component={Home} exact />
+    <Route component={Pagina404} />
+
+  </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-
